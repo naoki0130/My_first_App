f8b0a77e8f7257925732e14bbecdd1405c8ce798
naoki0130 <key1220931@gamil.com>
2020-04-25 18:26:18 +0900

    initial

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import ListView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
